f748c040a38bf52f371146b3b88230da0a7994b0
117503445 <t117503445@gmail.com>
2024-12-28 17:13:38 +0800

    fs watch
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.4
 
 require (
 	github.com/117503445/goutils v0.0.0-20241208164233-fe236427e42b // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/117503445/goutils"
+	"github.com/fsnotify/fsnotify"
 	"github.com/rs/zerolog/log"
 )
 
@@ -9,4 +10,42 @@ func main() {
 	goutils.InitZeroLog()
 
 	log.Info().Msg("Hello, World!")
+
+	// Create new watcher.
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create watcher")
+	}
+	defer watcher.Close()
+
+	// Start listening for events.
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Debug().Interface("event", event).Msg("event")
+				if event.Has(fsnotify.Write) {
+					log.Debug().Str("event", "modified").Str("file", event.Name).Msg("file modified")
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Error().Err(err).Msg("error")
+			}
+		}
+	}()
+
+	// Add a path.
+	err = watcher.Add("/tmp")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to add path")
+	}
+
+	// Block main goroutine forever.
+	<-make(chan struct{})
+
 }
