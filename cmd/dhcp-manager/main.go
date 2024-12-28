75c80834c3524da7c47d170a8700a3e71a9f5b31
117503445 <t117503445@gmail.com>
2024-12-28 17:55:31 +0000

    server
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- cmd/dhcp-manager/main.go
+++ cmd/dhcp-manager/main.go
@@ -13,58 +13,19 @@ func main() {
 	goutils.InitZeroLog()
 	log.Info().Msg("Hello, World!")
 
-	go func() {
-		server.NewServer().Start(8080)
-	}()
-
 	leaseChan := make(chan []*grpcgen.Lease)
-
 	go func() {
-		for leases := range leaseChan {
-			log.Info().Interface("leases", leases).Msg("Parsed leases")
-		}
+		server.NewServer(leaseChan).Start(8080)
 	}()
+
+	// go func() {
+	// 	for leases := range leaseChan {
+	// 		log.Info().Interface("leases", leases).Msg("Parsed leases")
+	// 	}
+	// }()
 
 	err := kea.ParseStream("/var/lib/kea/dhcp4.leases", leaseChan)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to parse stream")
 	}
-
-	// // Create new watcher.
-	// watcher, err := fsnotify.NewWatcher()
-	// if err != nil {
-	// 	log.Fatal().Err(err).Msg("Failed to create watcher")
-	// }
-	// defer watcher.Close()
-
-	// // Start listening for events.
-	// go func() {
-	// 	for {
-	// 		select {
-	// 		case event, ok := <-watcher.Events:
-	// 			if !ok {
-	// 				return
-	// 			}
-	// 			log.Debug().Interface("event", event).Msg("event")
-	// 			if event.Has(fsnotify.Write) {
-	// 				log.Debug().Str("event", "modified").Str("file", event.Name).Msg("file modified")
-	// 			}
-	// 		case err, ok := <-watcher.Errors:
-	// 			if !ok {
-	// 				return
-	// 			}
-	// 			log.Error().Err(err).Msg("error")
-	// 		}
-	// 	}
-	// }()
-
-	// // Add a path.
-	// err = watcher.Add("/tmp")
-	// if err != nil {
-	// 	log.Fatal().Err(err).Msg("Failed to add path")
-	// }
-
-	// // Block main goroutine forever.
-	// <-make(chan struct{})
-
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"net"
+	"sync"
 
 	"github.com/117503445/dhcp-manager/pkg/grpcgen"
 	"github.com/rs/zerolog/log"
@@ -12,14 +13,24 @@ import (
 
 type Server struct {
 	grpcgen.DHCPManagerServer
+	leaseChan <-chan []*grpcgen.Lease
+
+	streams     []grpc.ServerStream
+	streamsLock sync.RWMutex
 }
 
 func (s *Server) GetLeases(_ *emptypb.Empty, stream grpc.ServerStreamingServer[grpcgen.GetLeasesResponse]) error {
+	s.streamsLock.Lock()
+	defer s.streamsLock.Unlock()
+	s.streams = append(s.streams, stream)
 	return nil
 }
 
-func NewServer() *Server {
-	return &Server{}
+func NewServer(leaseChan <-chan []*grpcgen.Lease) *Server {
+	return &Server{
+		leaseChan: leaseChan,
+		streams:   make([]grpc.ServerStream, 0),
+	}
 }
 
 func (s *Server) Start(port int) {
@@ -30,6 +41,46 @@ func (s *Server) Start(port int) {
 	grpcS := grpc.NewServer()
 	grpcgen.RegisterDHCPManagerServer(grpcS, s)
 
+	go func() {
+		broadcast := func(leases []*grpcgen.Lease) {
+			var wg sync.WaitGroup
+
+			failedStreams := make(map[grpc.ServerStream]struct{})
+
+			s.streamsLock.RLock()
+			defer s.streamsLock.RUnlock()
+
+			for _, stream := range s.streams {
+				wg.Add(1)
+				go func(stream grpc.ServerStream) {
+					defer wg.Done()
+					// TODO: timeout
+					if err := stream.SendMsg(&grpcgen.GetLeasesResponse{Leases: leases}); err != nil {
+						log.Error().Err(err).Msg("Failed to send message")
+						failedStreams[stream] = struct{}{}
+					}
+				}(stream)
+			}
+			wg.Wait()
+
+			if len(failedStreams) > 0 {
+				s.streamsLock.Lock()
+				defer s.streamsLock.Unlock()
+
+				newStreams := make([]grpc.ServerStream, 0, len(s.streams)-len(failedStreams))
+				for _, stream := range s.streams {
+					if _, ok := failedStreams[stream]; !ok {
+						newStreams = append(newStreams, stream)
+					}
+				}
+				s.streams = newStreams
+			}
+		}
+
+		for leases := range s.leaseChan {
+			broadcast(leases)
+		}
+	}()
 	if err := grpcS.Serve(lis); err != nil {
 		log.Fatal().Err(err).Msg("Failed to serve")
 	}
