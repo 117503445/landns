c41aba1fd216e1eeb74795741b6036a654e15730
117503445 <t117503445@gmail.com>
2025-02-19 16:20:45 +0000

    build: 移除 Docker 构建中的代理设置
    - 注释掉 Dockerfile.builder 中的代理配置行
    - 删除 build-in-docker.sh 中的 protoc 和 go build 命令
    
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- scripts/build-in-docker.sh
+++ scripts/build-in-docker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 set -e
-sh
 protoc --go_out=. --go_opt=paths=source_relative --twirp_out=. ./pkg/rpcgen/landns.proto
 go build -o landns ./cmd/landns
 go build -o landns-agent ./cmd/landns-agent
